f3eefde15ebfad709db0d4969fd60f5b09fd511e
Nathan Nguyen <157441039+CvmuloSky@users.noreply.github.com>
2024-12-06 19:14:37 -0500

    day 5

--- Day 5/Day5Part2.java
+++ Day 5/Day5Part2.java
@@ -7,11 +7,14 @@ public class Day5Part2 {
     public static void main(String[] args) {
         String fileName = "Day 5/in.txt";
         int midSum = 0;
+        int fixedMidSum = 0;
         LinkedList<Integer> list = new LinkedList<>();
         String[] pageOrder = new String[0];
+        
         try {
             try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
                 String line;
+                // Parse the rules
                 while ((line = br.readLine()) != null && line.indexOf("|") != -1) {
                     String[] lines = line.split("\\|");
                     LinkedList<Integer> temp = new LinkedList<>();
@@ -22,19 +25,20 @@ public class Day5Part2 {
                         map.get(Integer.parseInt(lines[0])).add(Integer.parseInt(lines[1]));
                     }
                 }
+                
+                // Parse the updates
                 while ((line = br.readLine()) != null && line.indexOf(",") != -1) {
-                    pageOrder = line.split("\\,");
-                    // System.out.println(pageOrder.length);
+                    pageOrder = line.split(",");
                     if (isValid(pageOrder)) {
                         midSum += getMid(pageOrder);
                     } else {
-                        pageOrder = fixOrder(pageOrder);
-                        midSum += getMid(pageOrder);
+                        String[] fixedOrder = fixOrder(pageOrder);
+                        fixedMidSum += getMid(fixedOrder);
                     }
                 }
             }
-            System.out.println(map);
-            System.out.println("Mid sum: " + midSum);
+            System.out.println("Mid sum of valid updates: " + midSum);
+            System.out.println("Mid sum of fixed updates: " + fixedMidSum);
         } catch (IOException e) {
             System.err.println("Error reading file: " + e.getMessage());
         }
@@ -55,21 +59,28 @@ public class Day5Part2 {
     }
 
     public static String[] fixOrder(String[] arr) {
-        String[] corrected = new String[0];
-        for (int i = 0; i < arr.length; i++) {
-            int curr = Integer.parseInt(arr[i]);
-            for (int j = 0; j < i; j++) {
-                int prev = Integer.parseInt(arr[j]);
-                LinkedList<Integer> afters = map.get(curr);
-                if (afters != null && afters.contains(prev)) {
-                    
+        List<Integer> correctedList = new ArrayList<>(); 
+        for (String s : arr) {
+            int curr = Integer.parseInt(s);
+            int insertPosition = correctedList.size();
+            for (int j = 0; j < correctedList.size(); j++) {
+                int prev = correctedList.get(j);
+                LinkedList<Integer> afters = map.get(prev);
+                    if (afters != null && afters.contains(curr)) {
+                    insertPosition = j;
+                    break;
                 }
             }
+                correctedList.add(insertPosition, curr);
+        }
+        String[] corrected = new String[correctedList.size()];
+        for (int i = 0; i < correctedList.size(); i++) {
+            corrected[i] = String.valueOf(correctedList.get(i));
         }
         return corrected;
     }
-
     
+
     public static int getMid(String[] arr) {
         return Integer.parseInt(arr[arr.length / 2]);
     }
